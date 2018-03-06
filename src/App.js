aa0e2e99964989009f342d4ec0c138449f1d1b55
majorazero <daniel.cw.hsu@gmail.com>
2018-03-05 21:34:06 -0800

    tried using API fetchcall in the ComponentDidMount but it's not firing, possibly due to how the server is set up on the create react app, will go back to see what I can do after I figure out API calls in react in another repo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      cityTemp : []
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Let's try to fetch an API hahaha.</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -16,6 +20,20 @@ class App extends Component {
       </div>
     );
   }
+  ComponentDidMount(){
+    console.log(2);
+    let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
+    let city = "London";
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    fetch(url)
+    .then((result) => {
+      return JSON.parse(result);
+    }).then((data) => {
+      console.log(data);
+      console.log(3);
+    });
+  }
 }
 
 export default App;
+//module.exports = App;
